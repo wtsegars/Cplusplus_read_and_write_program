59407e4bc778480d7f580e7fd4dd1f164cdd5cff
Darkangel <wtsegars@mac.com>
2020-03-09 17:35:20 -0400

    Added new .cpp file

---
+++ read_write.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+struct ReadFrom {
+    ReadFrom () {
+
+    }
+};
+
+struct WriteTo {
+    WriteTo() {
+        
+    }
+};
+
+int main() {
+    char lower[] = "abc?e";
+    char upper[] = "ABC?E";
+    char* upper_ptr = upper; // Equivalant: &upper[0]
+
+    lower[3] = 'd'; // lower now contains a b c d e \0
+    upper_ptr[3] = 'D'; // upper now contains A B C D E \0
+
+    char letter_d = lower[3]; // letter_d equals 'd'
+    char letter_D = upper_ptr[3]; // letter_D equals 'D'
+
+    printf("lower: %s\nupper: %s", lower, upper);
+
+    lower[7] = 'g'; // super bad, you must never do this
+}
