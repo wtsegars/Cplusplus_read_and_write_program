2e09fdc1161eab779c8db76394d1874663a2bff5
Darkangel <wtsegars@mac.com>
2020-03-11 15:09:30 -0400

    Removed older functionality

--- read_write.cpp
+++ read_write.cpp
@@ -36,16 +36,7 @@ private:
 int main() {
     char lower[] = "abc?e";
     char upper[] = "ABC?E";
-    char* upper_ptr = upper; // Equivalant: &upper[0]
-
-    lower[3] = 'd'; // lower now contains a b c d e \0
-    upper_ptr[3] = 'D'; // upper now contains A B C D E \0
-
-    char letter_d = lower[3]; // letter_d equals 'd'
-    char letter_D = upper_ptr[3]; // letter_D equals 'D'
-
-    printf("lower: %s\nupper: %s", lower, upper);
-
+    
     WriteTo writeto(lower);
-    ReadFrom readfrom();
+    ReadFrom readfrom;
 }
